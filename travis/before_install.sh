b4b7323655c5d5d6b663b9e806dcd2855a9c98f3
JF Perini <fjperini@gmail.com>
2018-06-24 17:26:05 -0300

    Update before_install.sh

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -58,9 +58,5 @@ for f in ${CHANGED_FORMULAE};do
       ${HOMEBREW_PREFIX}/bin/pip3 install future mock nose2 numpy psycopg2 pyyaml
     fi
   fi
-  
-  brew install gdal2
-  
+
 done
-# Remove any left over lock or stray cache files
-brew cleanup
